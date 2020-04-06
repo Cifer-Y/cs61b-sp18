20ca378cdde2a504649c2168bfbad067c92f6ab7
Cifer-Y <cifery180236@gmail.com>
2020-04-06 22:35:55 +0800

    fix 1 pro in lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,10 +82,7 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         if(A == null) {
-            return B;
-        }
-        if(B == null) {
-            return A;
+            return null;
         }
 
         IntList p = A;
