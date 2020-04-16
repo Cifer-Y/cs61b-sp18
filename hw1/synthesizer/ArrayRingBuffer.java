ee6afce26e135c65cf8a97443e8e414661b80f87
Cifer-Y <cifery180236@gmail.com>
2020-04-16 21:58:45 +0800

    hw1

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,82 @@
+package synthesizer;
+import java.util.Iterator;
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        this.capacity = capacity;
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        rb = (T[]) new Object[capacity];
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    public void enqueue(T x) {
+        if (isFull()) {
+            throw new RuntimeException("Ring Buffer Overflow");
+        }
+        rb[last] = x;
+        fillCount++;
+        last = (last + 1) % capacity;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer Underflow");
+        }
+        T ret = rb[first];
+        rb[first] = null;
+        first = (first + 1) % capacity;
+        fillCount--;
+        return ret;
+
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    public T peek() {
+        return rb[first];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new KeyIterator();
+    }
+
+    public class KeyIterator implements Iterator<T> {
+        private int ptr;
+        public KeyIterator() {
+            ptr = first;
+        }
+        public boolean hasNext() {
+            return ptr != (last - 1 + capacity()) % capacity();
+        }
+        public T next() {
+            T ret = rb[ptr];
+            ptr = (ptr + 1) % capacity();
+            return ret;
+        }
+    }
+
+
+}
