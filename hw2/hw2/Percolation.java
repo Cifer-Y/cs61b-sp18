7f624524e85a97095910412d257041b69bdeeccc
Cifer-Y <cifery180236@gmail.com>
2020-04-20 19:12:46 +0800

    hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,83 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[][] sites;
+    private int width;
+    private int vtop;
+    private int openedSites = 0;
+    private WeightedQuickUnionUF WQUUF;
 
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IndexOutOfBoundsException("Illegal size");
+        }
+        sites = new boolean[N][N];
+        width = N;
+        WQUUF = new WeightedQuickUnionUF((width * width) + 2);
+        vtop = width * width;
+
+    }
+
+    public void open(int row, int col) {
+        if (row < 0 || row >= width || col < 0 || col > width) {
+            throw new IndexOutOfBoundsException("Illegal size");
+        }
+        sites[row][col] = true;
+        checkNeighbourAndConnect(row, col);
+        openedSites++;
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row >= width || col < 0 || col > width) {
+            throw new IndexOutOfBoundsException("Illegal size");
+        }
+        return sites[row][col];
+    }
+
+    public boolean isFull(int row, int col) {
+        return  WQUUF.connected(vtop, xyTo1D(row, col));
+    }
+
+    public int numberOfOpenSites() {
+        return openedSites;
+    }
+
+    public boolean percolates() {
+        for (int i = 0; i < width; i++) {
+            if (WQUUF.connected(vtop, xyTo1D(width - 1, i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int xyTo1D(int row, int col) {
+        return row * width + col;
+    }
+
+    private void checkNeighbourAndConnect(int row, int col) {
+        int current1D = xyTo1D(row, col);
+        if (row == 0) {
+            WQUUF.union(current1D, vtop);
+        }
+
+        int topRow = row - 1;
+        int rightCol = col + 1;
+        int bottomRow = row + 1;
+        int leftCol = col - 1;
+
+        if (topRow >= 0 && isOpen(topRow, col)) {
+            WQUUF.union(current1D, xyTo1D(topRow, col));
+        }
+        if (rightCol < width && isOpen(row, rightCol)) {
+            WQUUF.union(current1D, xyTo1D(row, rightCol));
+        }
+        if (bottomRow < width && isOpen(bottomRow, col)) {
+            WQUUF.union(current1D, xyTo1D(bottomRow, col));
+        }
+        if (leftCol >= 0 && isOpen(row, leftCol)) {
+            WQUUF.union(current1D, xyTo1D(row, leftCol));
+        }
+    }
 }
+
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,45 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
+    private int trial;
+    private double mean;
+    private double[] xTrials;
+    private double stddev;
 
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        trial = T;
+        int col, row;
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        xTrials = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation temp = pf.make(N);
+            while (!temp.percolates()) {
+                col = StdRandom.uniform(N) + 1;
+                row = StdRandom.uniform(N) + 1;
+                temp.open(row, col);
+            }
+            xTrials[i] = (double) temp.numberOfOpenSites() / (N * N);
+        }
+        mean = StdStats.mean(xTrials);
+        stddev = StdStats.stddev(xTrials);
+    }
+
+    public double mean() {
+        return mean;
+    }
+    public double stddev() {
+        return stddev;
+    }
+
+    public double confidenceLow() {
+        return mean - 1.96 * stddev / Math.sqrt(trial);
+    }
+
+    public double confidenceHigh() {
+        return mean + 1.96 * stddev / Math.sqrt(trial);
+    }
 }
